cce84a6eecd6b672473205e29a2e315fde93c3f9
souvik1232 <souvik199831@gmail.com>
2020-11-19 20:14:42 +0530

    Refractored:a new logic

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,17 +1,35 @@
 #! /bin/bash
-t=10
+t=0
 hc=0
 tc=0
-while(( t != 0 ))
+while(( t == 0 ))
 do
 	ran=$(( $RANDOM%2 ))
 	if(( $ran == 0 ))
 	then
 		(( hc++ ))
+		if(( $hc == 21 ))
+		then
+			break
+		fi
 	else
 		(( tc++ ))
+		if(( $tc == 21 ))
+		then
+			break
+		fi
 	fi
-	(( t-- ))
 done
-echo "Head won---> $hc"
-echo "Tail won---> $tc"
+if(( $hc == $tc ))
+then
+	echo "Tie"
+else
+	echo "Win"
+	if(( $hc > $tc ))
+	then
+		echo "Head won---> $(( $hc - $tc ))"
+	else
+		echo "Tail won---> $(( $tc - $hc ))"
+	fi
+fi
+
